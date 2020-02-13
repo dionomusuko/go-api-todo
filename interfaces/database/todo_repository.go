b49ea5e46ec8b50454ee8e2e5e6fe4e2e5043029
dionomusuko <ruby.school114@gmail.com>
2020-02-13 23:16:11 +0900

    Add database

---
+++ interfaces/database/todo_repository.go
@@ -0,0 +1,27 @@
+package database
+
+type TodoRepository struct {
+	SqlHandler
+}
+
+func (repo *TodoRepository) FindById(id int) (todo domain.Todo, err error) {
+	if err = repo.Find(&todo, id).Error; err != nil {
+		return
+	}
+	return
+}
+
+func (repo *TodoRepository) FindAll() (todos domain.Todos, err error) {
+	if err = repo.Find(&todos).Error; err != nil {
+		return
+	}
+	return
+}
+
+func (repo *TodoRepository) Store(u domain.Todo) (user domain.Todo, err error) {
+	if err = repo.Create(&u).Error; err != nil {
+		return
+	}
+	todo = u
+	return
+}
